18db607135b3b70d24a8e871297a3a95c0d35776
Artem <artempozitiv56@gmail.com>
2022-02-21 18:12:57 +0200

    Add Interactive Mode

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	//"fmt"
+	"bufio"
+	"fmt"
 	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func equationSolver(values []float64) []float64 {
+
 	var roots []float64
 
 	var (
@@ -26,6 +31,41 @@ func equationSolver(values []float64) []float64 {
 	}
 
 	return roots
+}
+
+func interactiveMode() []float64 {
+
+	var param []float64
+	var args = []string{"a", "b", "c"}
+
+	var reader = bufio.NewReader(os.Stdin)
+
+	for i := 0; i != 3; i++ {
+		fmt.Printf("%s: ", args[i])
+		var parameters, err = reader.ReadString('\n')
+
+		if err != nil {
+			fmt.Println("Error. Failed to read input. Please try again", err)
+			continue
+		}
+
+		parameters = strings.TrimSuffix(parameters, "\r\n")
+		parameterFloat, err := strconv.ParseFloat(parameters, 64)
+
+		if err != nil {
+			fmt.Printf("Error. Expected a valid real number, got %s instead\n", parameters)
+			continue
+		}
+
+		if parameterFloat == 0 && len(param) == 0 {
+			fmt.Print("Error. a cannot be 0\n")
+			continue
+		}
+
+		param = append(param, parameterFloat)
+	}
+
+	return param
 }
 
 func main() {
